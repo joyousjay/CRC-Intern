f20bca3e4ea7116b9b300da489db33b37cc07a6f
Janet Majekodunmi <janetmajek@gmail.com>
2021-05-07 05:27:42 +0000

    upload csv file with dois and code that works, got help from chegg as battling through api requests and reading files

--- main.py
+++ main.py
@@ -1,47 +1,45 @@
 import requests
-import jsons
-import json
 
 file=input("enter file name : ")
-fileReader=open(file,"r")
-l=fileReader.read().strip("\n").split(',')
+fileOpener=open(file,"r")
+fileReader=fileOpener.read().strip("\n").split(',')
 
 #loop through file
-for i in l:
+for i in fileReader:
     print("details for DOI {}".format(i))
 
-# api-endpoint
-# i is the single DOI
-URL = "https://api.crossref.org/works/"+i 
-r = requests.get(url = URL)
+    # api-endpoint
+    # i is the single DOI
+    URL = "https://api.crossref.org/works/"+i 
+    r = requests.get(url = URL)
 
-# extracting data in json format
-data = r.json()
+    # extracting data in json format
+    data = r.json()
 
-print("title:",end="  ")
-title=data['message']['title'][0]
-print(title)
-authors=[]
+    print("title:",end="  ")
+    title=data['message']['title'][0]
+    print(title)
+    authors=[]
 
-print("author(s):",end="\n\t")
-for i in range(len(data['message']['author'])):
-    name=data['message']['author'][i]['given'] + data['message']['author'][i]['family']
-    authors.append(name)
+    print("author(s):",end="\n\t")
+    for i in range(len(data['message']['author'])):
+        name=data['message']['author'][i]['given'] + data['message']['author'][i]['family']
+        authors.append(name)
 
-print("\n\t".join(authors))
+    print("\n\t".join(authors))
 
 
-print("published date:",end="  ")
-get_date_parts=data['message']['issued']['date-parts']
-#print(get_date_parts)
-if(len(get_date_parts[0])==1):
-    date_issued=str(get_date_parts[0][0])
-elif(len(get_date_parts[0])==2):
-    date_issued=str(get_date_parts[0][1])+"-"+str(get_date_parts[0][0])
+    print("published date:",end="  ")
+    get_date_parts=data['message']['issued']['date-parts']
+    #print(get_date_parts)
+    if(len(get_date_parts[0])==1):
+        date_issued=str(get_date_parts[0][0])
+    elif(len(get_date_parts[0])==2):
+        date_issued=str(get_date_parts[0][1])+"-"+str(get_date_parts[0][0])
 
-else:    
-    date_issued=str(get_date_parts[0][2])+"-"+str(get_date_parts[0][1])+"-"+str(get_date_parts[0][0])
+    else:    
+        date_issued=str(get_date_parts[0][2])+"-"+str(get_date_parts[0][1])+"-"+str(get_date_parts[0][0])
 
-print(date_issued)
-print()
-print()
+    print(date_issued)
+    print()
+    print()
